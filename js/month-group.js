5985ed71bed5a6d19e16e3cc7a54ed25c177398f
Evan Hensleigh <futuraprime@gmail.com>
2016-02-02 17:34:43 +0000

    Added month group.

---
+++ js/month-group.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import BoundedSVG from './bounded-svg.js';
+import colours from './econ_colours.js';
+import { mapToObject, Im } from './utilities.js';
+
+export const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+
+
+class MonthBar extends React.Component {
+  static get defaultProps() {
+    return {
+      specialColour : colours.grey[5],
+      duration : 150,
+      rectColour : colours.aquamarine[1]
+    };
+  }
+  constructor(props, ...args) {
+    super(props, ...args);
+    this.state = mapToObject(this.transitionableProps, props);
+    this.animationStep = this.animationStep.bind(this);
+  }
+  componentWillReceiveProps(newProps) {
+    d3.timer(this.animationStep);
+    this.interpolators = mapToObject(this.transitionableProps, k => d3.interpolate(this.state[k], newProps[k]));
+  }
+  animationStep(ms) {
+    var progress = Math.min(1, ms / this.props.duration);
+    this.setState(mapToObject(this.transitionableProps, k => this.interpolators[k](progress)));
+    // if we're done, end the animation
+    if(progress === 1) { return true; }
+    return false;
+  }
+  render() {
+    // 'special' is a bye for superdelegates, which are assigned to the
+    // 1st of January
+    var special = this.props.label === 'JAN';
+    var rectProps = {
+      fill : special ? this.props.specialColour : this.props.rectColour,
+      height : this.state.height,
+      width : this.state.width,
+      x : this.state.x,
+      y : this.state.y
+    };
+    var textProps = {
+      className : 'month-label',
+      x : this.state.x + this.state.width / 2,
+      y : 24,
+      textAnchor : 'middle'
+    };
+
+    var textEl = special ? null :
+      (<text {...textProps}>{this.props.label}</text>);
+
+    return (<g>
+      <rect {...rectProps}/>
+      {textEl}
+    </g>);
+  }
+}
+MonthBar.prototype.transitionableProps = ['width', 'height', 'x', 'y'];
+
+export default class MonthGroup extends BoundedSVG {
+  static get defaultProps() {
+    return Im.extend(super.defaultProps, {
+      duration : 300
+    });
+  }
+  render() {
+    var scale = this.props.scale;
+    var cumulative = this.leftBound - 2;
+    var monthElements = this.props.monthSections.map((d,idx) => {
+      var width = scale(d) - scale(0);
+      var monthProps = {
+        duration : this.props.duration,
+        height : 15,
+        width : width - 2,
+        x : cumulative,
+        y : this.topBound,
+        key : months[idx].toUpperCase(),
+        label : months[idx].toUpperCase()
+      }
+      cumulative += width;
+      return (<MonthBar {...monthProps} />);
+    });
+
+    return (<g>
+      {monthElements}
+    </g>);
+  }
+}
--- js/us-primary.js
+++ js/us-primary.js
@@ -9,6 +9,7 @@ import colours from './econ_colours.js';
 
 import PrimariesKey from './primaries-key.js';
 import DateLabel from './date-label.js';
+import MonthGroup from './month-group.js';
 
 import BoundedSVG from './bounded-svg.js';
 
@@ -84,9 +85,6 @@ const PRIMARIES = {
                '#866b67','#ccaf88','#fed700','#f0ae00','#cd881d','#ea5f10']
   }
 }
-
-var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
 
 class USPrimaryElement extends React.Component {
   constructor(props, ...args) {
@@ -184,89 +182,6 @@ class USPrimaryElement extends React.Component {
   }
 }
 USPrimaryElement.prototype.transitionableProps = ['x', 'y'];
-
-class MonthBar extends React.Component {
-  static get defaultProps() {
-    return {
-      specialColour : colours.grey[5],
-      duration : 150,
-      rectColour : colours.aquamarine[1]
-    };
-  }
-  constructor(props, ...args) {
-    super(props, ...args);
-    this.state = mapToObject(this.transitionableProps, props);
-    this.animationStep = this.animationStep.bind(this);
-  }
-  componentWillReceiveProps(newProps) {
-    d3.timer(this.animationStep);
-    this.interpolators = mapToObject(this.transitionableProps, k => d3.interpolate(this.state[k], newProps[k]));
-  }
-  animationStep(ms) {
-    var progress = Math.min(1, ms / this.props.duration);
-    this.setState(mapToObject(this.transitionableProps, k => this.interpolators[k](progress)));
-    // if we're done, end the animation
-    if(progress === 1) { return true; }
-    return false;
-  }
-  render() {
-    // 'special' is a bye for superdelegates, which are assigned to the
-    // 1st of January
-    var special = this.props.label === 'JAN';
-    var rectProps = {
-      fill : special ? this.props.specialColour : this.props.rectColour,
-      height : this.state.height,
-      width : this.state.width,
-      x : this.state.x,
-      y : this.state.y
-    };
-    var textProps = {
-      className : 'month-label',
-      x : this.state.x + this.state.width / 2,
-      y : 24,
-      textAnchor : 'middle'
-    };
-
-    var textEl = special ? null :
-      (<text {...textProps}>{this.props.label}</text>);
-
-    return (<g>
-      <rect {...rectProps}/>
-      {textEl}
-    </g>);
-  }
-}
-MonthBar.prototype.transitionableProps = ['width', 'height', 'x', 'y'];
-
-class MonthGroup extends BoundedSVG {
-  static get defaultProps() {
-    return Im.extend(super.defaultProps, {
-      duration : 300
-    });
-  }
-  render() {
-    var scale = this.props.scale;
-    var cumulative = this.leftBound - 2;
-    var monthElements = this.props.monthSections.map((d,idx) => {
-      var width = scale(d) - scale(0);
-      var monthProps = {
-        duration : this.props.duration,
-        height : 15,
-        width : width - 2,
-        x : cumulative,
-        y : this.topBound,
-        key : months[idx].toUpperCase(),
-        label : months[idx].toUpperCase()
-      }
-      cumulative += width;
-      return (<MonthBar {...monthProps} />);
-    });
-
-    return (<g>
-      {monthElements}
-    </g>);
-  }
-}
 
 var primaryGraphPadding = 5;
 class PrimaryGraph extends BoundedSVG {
