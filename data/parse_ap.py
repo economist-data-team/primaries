d60dc15abc93e15bfc879c425b5679f3ad0053b2
Evan Hensleigh <futuraprime@gmail.com>
2016-05-11 10:34:53 +0100

    Adding AP fetcher.

---
+++ data/parse_ap.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import requests
+import csvkit as csv
+
+CANDIDATE_KEYS = {
+    "O'Malley" : "omalley",
+    "Clinton" : "clinton",
+    "Sanders" : "sanders",
+    "Huckabee" : "huckabee",
+    "Cruz" : "cruz",
+    "Kasich" : "kasich",
+    "Carson" : "carson",
+    "Trump" : "trump",
+    "Rubio" : "rubio",
+    "Christie" : "christie",
+    "Bush" : "bush",
+    "Fiorina" : "fiorina",
+    "Paul" : "paul",
+    "Uncommitted" : "uncommitted",
+}
+
+URL = 'https://interactives.ap.org/interactives/2016/delegate-tracker/live-data/data/delegates-delsuper.json'
+r = requests.get(URL)
+supers = r.json()['delSuper']['del']
+
+for party in supers:
+    states = party['State']
+    for state in states:
+        candidates = state['Cand']
+        for cand in candidates:
+            cand['pTot'] = int(cand['dTot']) - int(cand['sdTot'])
+
+dem, rep = [p['State'] for p in supers]
+
+dem_dels = [dict([('state', s['sId']), ('party', 'DEM')] + [('%s_del' % CANDIDATE_KEYS[c['cName']], c['pTot']) for c in s['Cand']]) for s in dem]
+rep_dels = [dict([('state', s['sId']), ('party', 'GOP')] + [('%s_del' % CANDIDATE_KEYS[c['cName']], c['pTot']) for c in s['Cand']]) for s in rep]
+
+dels = dem_dels + rep_dels
+
+with open('delegates_ap.csv', 'w+') as f:
+    writer = csv.DictWriter(f, fieldnames=set(dem_dels[0].keys()).union(set(rep_dels[0].keys())))
+    writer.writeheader()
+    writer.writerows(dels)
